16d8aa7944801aefac23483dad20de4642fd52c1
plauer <plauer411@gmail.com>
2014-11-26 17:40:26 -0700

    first test passes

--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,27 @@
 var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
+var Link = require('./link.js')
 
 var User = db.Model.extend({
+  tableName: 'users',
+  hasTimestamps: true,
+  defaults: {
+    // visits: 0
+  },
+  // clicks: function() {
+  //   return this.hasMany(Click);
+  // },
+  // initialize: function(){
+  //   this.on('creating', function(model, attrs, options){
+  //     var shasum = crypto.createHash('sha1');
+  //     shasum.update(model.get('url'));
+  //     model.set('code', shasum.digest('hex').slice(0, 5));
+  //   });
+  // },
+  links: function() {
+    return this.hasMany(Link);
+  }
 });
 
 module.exports = User;
